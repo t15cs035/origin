b4c821324f9d81739812ba437cbe6260672ba988
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2017-10-16 16:08:49 +0900

    [modify] HandクラスのメソッドとHandTestクラスを追加

--- src/Hand.java
+++ src/Hand.java
@@ -8,7 +8,7 @@ public class Hand {
 
 	/** プロパティ */
 	/** 手札にあるカードを保持するためのリスト */
-    private ArrayList hand_ = new ArrayList();
+    private ArrayList<Card> hand_ = new ArrayList<Card>();
 
     /** メソッド */
     /**
@@ -18,6 +18,9 @@ public class Hand {
      */
     public void addCard(Card card)
     {
+    	// カードをリストの最後に追加する
+    	// ArrayList クラスの add メソッドを使う
+    	hand_.add(card);
     }
 
     /**
@@ -27,6 +30,8 @@ public class Hand {
      */
     public Card pickCard()
     {
+    	Card card = ****; // hand_ の先頭からカードを取り出す
+    	return card;
     }
 
     /**
@@ -37,6 +42,40 @@ public class Hand {
      */
     public Card[] findSameNumberCard()
     {
+        int numberOfCards = hand_.size();  // 手札の数
+        Card[] sameCards = null;           // 同じ数のカード ← 返り値として使う
+
+        // 手札にカードが1枚もない場合は何もしない
+        if (numberOfCards > 0)
+        {
+            // 最後に追加されたカードを取得する
+            int lastIndex = numberOfCards - 1;
+            Card lastAddedCard = (Card) hand_.get(lastIndex);
+
+            // 最後に追加されたカードの数字を取得する
+            int lastAddedCardNum = lastAddedCard.getNumber();
+
+            // 先頭のカードから順に lastAddedCardNum と同じ番号のカードを探す
+            for (int index = 0; index < lastIndex; index++)
+            {
+                Card card = (Card) hand_.get(index);
+                if (card.getNumber() == lastAddedCardNum)
+                {
+                    // 最後に追加されたカードと同じカードが見つかった場合
+                    // 見つかった組み合わせをsameCardsに格納し、ループを抜ける
+                    sameCards = new Card[2];
+                    sameCards[0] = (Card) hand_.remove(lastIndex);
+                    sameCards[1] = (Card) hand_.remove(index);
+
+                    break;
+                }
+
+                // 同じ数の組み合わせが見つからなかった場合、
+                // sameCardsはnullのままとなる。
+            }
+        }
+
+        return sameCards;
     }
 
     /**
@@ -46,6 +85,7 @@ public class Hand {
      */
     public int getNumberOfCards()
     {
+    	return hand_.size();
     }
 
     /**
@@ -53,6 +93,39 @@ public class Hand {
      */
     public void shuffle()
     {
+    	// 手札の枚数を取得
+    	int number = hand_.size();
+    	
+    	// カードを抜き出す位置
+    	int pos;
+    	
+    	// ランダムな位置のカードを抜き取って、最後に加える動作を n 回繰り返す
+    	for (int count = 0; count < n; count++)
+    	{
+    		pos = (int)(Math.random() * number); // pos は 0 から number-1 までの数
+    		// pos の位置からカードを抜いて
+    		// そのカードを最後に加える
+    	}
     }
 
+    /**
+     * 手札を画面に出力する（追加）
+     */
+    public String toString()
+    {
+    	// 先頭のカードから末尾のカードまでの toString 出力を " "（スペース）で区切った文字列を返す
+    	StringBuffer string = new StringBuffer();
+    	
+    	int size = hand_.size();
+    	if (size > 0)
+    	{
+    		for (int index = 0; index < size; index++)
+    		{
+    			Card card = (Card)hand_.get(index);
+    			string.append(card);
+    			string.append(" ");
+    		}
+    	}
+    	return string.toString();
+    }
 }
---
+++ src/HandTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class HandTest {
+
+	@Test
+	public void testAddCard() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testPickCard() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testFindSameNumberCard() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetNumberOfCards() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testShuffle() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testToString() {
+		fail("まだ実装されていません");
+	}
+
+}

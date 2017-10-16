ddd60e5c0b9065f5b4008464a4185afdb7ec6340
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2017-10-16 16:15:51 +0900

    [modify] Tableクラスのメソッドを一部追加（未完成）

--- src/Table.java
+++ src/Table.java
@@ -18,5 +18,14 @@ public class Table {
      */
     public void disposeCard(Card card[])
     {
+    	for (int index=0; index < card.length; index++ )
+    	{
+    		// 捨てられたカードを表示する
+    		System.out.print(card[index] + " ");
+    	}
+    	System.out.println("を捨てました");
+    	
+    	// 捨てられたカードは Table クラスの disposedCards_ に追加
+    	disposedCards_.***;
     }
 }
